f5c7a4a67359feca194c6cfd7bacc9e3cb468c3a
JessMelo20 <jessicarcmelo20@gmail.com>
2023-07-25 07:51:51 -0300

    atualizacao javascript

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,28 @@
-event.preventDefault();
-        const target = event.target.getAttribute("href");
-        const offsetTop = document.querySelector(target).offsetTop;
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTopButton = document.querySelector('.back-to-top');
 
-        window.scroll({
-          top: offsetTop,
-          behavior: "smooth"
+  function handleScroll() {
+    if (window.pageYOffset > 300) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+
+  backToTopButton.addEventListener('click', () => {
+    const scrollDuration = 300;
+
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  });
 });
+
     
